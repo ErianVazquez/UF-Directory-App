efa0a918ec64746e782fe6a8c17aa01b0db38bf4
Erian Vazquez <erian.vazquez@gmail.com>
2016-01-14 22:28:45 -0500

    added table and delete function figuring other things out

--- listingController.js
+++ listingController.js
@@ -1,14 +1,39 @@
-angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
+angular.module('listings').controller('ListingsController', ['$scope', 'Listings',
   function($scope, Listings) {
+    $scope.newItem = '';
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
 
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
+    /*
+      Implement these functions in the controller to make your application function
+      as described in the assignment spec.
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+
+     var defineNewListing = function (firstString, secondString) {
+       var string = '{"code": "'+firstString+'","name": "'+firstString+'"}';
+       return string;
+     }
+
+    $scope.addListing = function() {
+
+      var input = defineNewListing($scope.newCode, $scope.newName);
+
+      console.log(input);
+
+      $scope.listings.push(input);
+
+      // listings.entries.push
+      //
+      // $scope.listings.push($scope.newCode);
+      // $scope.newItem = '';
+    };
+    $scope.deleteListing = function(index) {
+      console.dir($scope.listings[index]);
+      $scope.listings.splice(index, 1);
+    };
+
+    $scope.showDetails = function(index) {
+
+    };
   }
 ]);
